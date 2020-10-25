91bb6b398fb4be7b8ba4dea92bc617d45e06218b
Muhammad Iqbal IPB <53424596+iqbal-ipb@users.noreply.github.com>
2020-10-25 12:50:59 +0700

    Add files via upload

---
+++ 2P.py
@@ -0,0 +1,24 @@
+import math
+
+def rata(data):
+    jumlah = 0.0
+    for i in ...(a)....:
+        jumlah = jumlah + i
+    rata = jumlah/len(...(b)...)
+    return rata
+
+
+def sd(data):
+    jumlah2 = 0
+    for i in data:
+        jumlah2 = jumlah2 + (i - ....(c)...(data))**2
+    ..(d)..... = math.sqrt(jumlah2/(len(data)-1))
+    return sigma
+
+
+a = [1, 2, 3, 4, 5, 6, 7, 10, 18, 9]
+b = [2, 3, 5]
+c = [3, 4, 5]
+
+print(rata(a))
+print(sd(b))
