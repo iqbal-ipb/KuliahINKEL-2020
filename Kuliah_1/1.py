c576ea2048e775a9057ba0ae22a5b4d8620e425f
Muhammad Iqbal IPB <53424596+iqbal-ipb@users.noreply.github.com>
2020-10-25 13:03:05 +0700

    Add files via upload

---
+++ Kuliah_1/1.py
@@ -0,0 +1,12 @@
+x = 1
+x = 1.2
+kata1 = "variabel huruf"
+kata2 = 'ini juga bentuk huruf'
+print(kata2+' ' + kata1)
+y = 100 + x
+print(y)
+y = 20 * x
+print(y)
+z = y ** 2
+print(z)
+print(kata2 * 2)
